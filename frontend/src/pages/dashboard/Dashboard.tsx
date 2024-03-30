f8d05ea9112b533b3eb5d00b1018185aa57481d7
DT VITHANA <dhanukavithana@gmail.com>
2024-03-30 15:29:52 +0530

    create-user-dashboard-component

---
+++ frontend/src/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,46 @@
+import { Box, Container, Grid, Grow } from '@mui/material';
+import DashButton from './dashButton';
+
+function Dashboard() {
+  return (
+    <>
+    <Container maxWidth="lg">
+      <Grid
+        width='1500px'
+        paddingTop='20px'
+        container
+        direction="row"
+        //justifyContent="center"
+        alignItems="stretch"
+        spacing={5}
+        
+      >
+        <Grid item lg={7.5} xs={12}>
+            <Grow in={true} style={{ transformOrigin: '0 0 0' }} {...(true ? { timeout: 700 } : {})}>  
+                <Box borderRadius ='15px' sx={{ boxShadow: 3}}>
+                
+                </Box>
+            </Grow>    
+        </Grid>
+        <Grid item lg={3} xs={12}>
+            <Grow in={true} style={{ transformOrigin: '0 0 0' }} {...(true ? { timeout: 1200 } : {})}> 
+                <Box borderRadius ='15px' sx={{boxShadow: 3}}>
+                  
+                </Box>
+            </Grow>
+        </Grid>
+        <Grid item lg={10.1} xs={12}>
+          <Grow in={true} style={{ transformOrigin: '0 0 0' }} {...(true ? { timeout: 1600 } : {})}>   
+            <Box borderRadius ='15px' sx={{boxShadow: 3}}>
+               <DashButton/>  
+            </Box>
+          </Grow>  
+        </Grid>
+      </Grid>
+    </Container>
+   
+  </>  
+  )
+}
+
+export default Dashboard
