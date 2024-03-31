9601758e5c81a2e83a157123924fe11b8285b542
DT VITHANA <dhanukavithana@gmail.com>
2024-03-31 18:29:42 +0530

    create-course-dashboard

---
+++ frontend/src/pages/course/CourseDashboard.tsx
@@ -0,0 +1,48 @@
+import { Box, Container, Grid, Grow } from '@mui/material';
+import CourseList from './StudentList';
+import DashButton from '../dashboard/dashButton';
+
+
+function CourseDashboard() {
+  return (
+    <>
+    <Container maxWidth="lg">
+      <Grid
+        width='75rem'
+        paddingTop='20px'
+        container
+        direction="row"
+        //justifyContent="center"
+        alignItems="stretch"
+        spacing={5}
+        
+      >
+        <Grid item lg={7.5} xs={12}>
+            <Grow in={true} style={{ transformOrigin: '0 0 0' }} {...(true ? { timeout: 700 } : {})}>  
+                <Box borderRadius ='15px' sx={{ boxShadow: 3}}>
+                   <CourseList/>
+                </Box>
+            </Grow>    
+        </Grid>
+        <Grid item lg={3.5} xs={13}>
+            <Grow in={true} style={{ transformOrigin: '0 0 0' }} {...(true ? { timeout: 1200 } : {})}> 
+                <Box borderRadius ='15px' sx={{boxShadow: 3}}>
+            
+                </Box>
+            </Grow>
+        </Grid>
+        <Grid item lg={10.1} xs={12}>
+          <Grow in={true} style={{ transformOrigin: '0 0 0' }} {...(true ? { timeout: 1600 } : {})}>   
+            <Box borderRadius ='15px' sx={{boxShadow: 3}}>
+               <DashButton/>  
+            </Box>
+          </Grow>  
+        </Grid>
+      </Grid>
+    </Container>
+   
+  </>  
+  )
+}
+
+export default CourseDashboard
