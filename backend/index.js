7d79ae7238e1cca17113b1cad5152a1c5e3fb4e8
DT VITHANA <dhanukavithana@gmail.com>
2024-03-31 22:36:52 +0530

    create-quries-for-add-update-for-courses-and-students

--- backend/index.js
+++ backend/index.js
@@ -35,6 +35,35 @@ app.post('/students', (req, res) => {
 })
 });
 
+app.put('/students/:id', (req, res) => {
+    const { fName, lName, address, birthday, degree, course1, course2, course3, course4 } = req.body;
+    const sql = `UPDATE students SET fName = ?, lName = ?, address = ?, birthday = ?, degree = ?, course1 = ?, course2 = ?, course3 = ?, course4 = ? WHERE id = ?`;
+    const values = [fName, lName, address, birthday, degree, course1, course2, course3, course4, req.params.id];
+    
+    db.query(sql, values, (err, result) => {
+        if(err) throw err;
+        return res.json(result);
+})
+});
+
+app.get('/courses', (req, res) => {
+    const sql = 'SELECT * FROM courses';
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        return res.json(result);
+    })
+});
+
+app.put('/courses/:id', (req, res) => {
+    const { courseName, courseId } = req.body;
+    const sql = `UPDATE courses SET courseName = ?, courseId = ? WHERE id = ?`;
+    const values = [courseName, courseId, req.params.id];
+    
+    db.query(sql, values, (err, result) => {
+        if(err) throw err;
+        return res.json(result);
+    });
+});
 
 app.listen(8081, () => {
     console.log('listening on port 8081')
