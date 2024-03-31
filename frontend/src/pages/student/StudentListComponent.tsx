660f942332b212638ae975e1b0b137420c807d88
DT VITHANA <dhanukavithana@gmail.com>
2024-03-31 09:27:10 +0530

    remove-dummy-data-from-studentComponentList

--- frontend/src/pages/student/StudentListComponent.tsx
+++ frontend/src/pages/student/StudentListComponent.tsx
@@ -8,25 +8,15 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { Button } from '@mui/material';
 
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
-) {
-  return { name, calories, fat, carbs, protein };
-}
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+interface StdProps{
+   data: string
+  }
 
-export default function StudentListComponent() {
+
+export default function StudentListComponent({data}: StdProps) {
+  
+
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -38,16 +28,16 @@ export default function StudentListComponent() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {data?.map((row) => (
             <TableRow
               key={row.name}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                {row.name}
+                {row._id}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.name}</TableCell>
+              <TableCell align="right">{row.index}</TableCell>
               <TableCell align="right"> 
                     <Button sx={{
                         bgcolor: '#09bd0c'+ ' !important', 
