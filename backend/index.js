a69e77fd72989efb920e867eaff2c11f95dad881
DT VITHANA <dhanukavithana@gmail.com>
2024-03-31 21:56:30 +0530

    create-nodeBackend

---
+++ backend/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+
+const db = mysql.createConnection({ 
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'student'
+});   
+
+const app = express();
+app.use(cors());
+
+app.get('/', (req, res) => {
+   return res.json('From backend');
+})
+
+app.get('/students', (req, res) => {
+    const sql = 'SELECT * FROM students';
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        return res.json(result);
+    })
+})
+
+app.post('/students', (req, res) => {
+    const { fName, lName, address, birthday, degree, course1, course2, course3, course4 } = req.body;
+    const sql = `INSERT INTO students (fName, lName, address, birthday, degree, course1, course2, course3, course4) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+    const values = [fName, lName, address, birthday, degree, course1, course2, course3, course4];
+    
+    db.query(sql, values, (err, result) => {
+        if(err) throw err;
+        return res.json(result);
+})
+});
+
+
+app.listen(8081, () => {
+    console.log('listening on port 8081')
+})   
